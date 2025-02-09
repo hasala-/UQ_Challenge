9fe7c025ae925ca04860e6ebec101864779fb38f
Hasala Peiris <hasala@gmail.com>
2025-02-09 17:24:16 +1000

    interim2

--- run_model.py
+++ run_model.py
@@ -1,11 +1,11 @@
 import subprocess
 
 # input file path
-input_file_path = './input_new.txt'
+input_file_path = './input3.txt'
 
 
 # Step 3: Run the executable and capture its output
-exe_path = './local_model_windows.exe'  # Path to the executable in the current folder
+exe_path = 'local_model_windows.exe'  # Path to the executable in the current folder
 command = [exe_path, input_file_path]
 
 print('Simulation executable has been called.')
